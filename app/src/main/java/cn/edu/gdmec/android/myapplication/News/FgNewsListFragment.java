9a74d102434fbd549a9865ae2a730a2edcf47dc5
shiyifei <806987963@qq.com>
2018-05-28 09:41:40 +0800

    第2次

--- app/src/main/java/cn/edu/gdmec/android/myapplication/News/FgNewsListFragment.java
+++ app/src/main/java/cn/edu/gdmec/android/myapplication/News/FgNewsListFragment.java
@@ -3,12 +3,15 @@ package cn.edu.gdmec.android.myapplication.News;
 import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.List;
 import java.util.TimerTask;
 
 import cn.edu.gdmec.android.myapplication.Bean.NewsBean;
@@ -20,13 +23,16 @@ public class FgNewsListFragment extends Fragment implements INewsView {
 
     private NewsPresenter presenter;
     private int type;
-    private TextView tv_news;
     private SwipeRefreshLayout srl_news;
+    private RecyclerView rv_news;
+    private ItemNewsAdapter adapter;
+    private List<NewsBean.Bean> newsBeanList;
+    private TextView tv_news_list;
 
-    public static FgNewsListFragment newInstance(int type){
-        Bundle args=new Bundle();
-        FgNewsListFragment fragment=new FgNewsListFragment();
-        args.putInt("type",type);
+    public static FgNewsListFragment newInstance(int type) {
+        Bundle args = new Bundle();
+        FgNewsListFragment fragment = new FgNewsListFragment();
+        args.putInt("type", type);
         fragment.setArguments(args);
         return fragment;
     }
@@ -34,62 +40,62 @@ public class FgNewsListFragment extends Fragment implements INewsView {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fg_news_list, null);
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        type=getArguments().getInt("type");
-        tv_news = view.findViewById(R.id.tv_news);
-        srl_news=view.findViewById(R.id.srl_news);
+        type = getArguments().getInt("type");
+        presenter = new NewsPresenter(this);
+        rv_news = view.findViewById(R.id.rv_news);
+        adapter = new ItemNewsAdapter(getActivity());
+        tv_news_list = view.findViewById(R.id.tv_news_list);
+        srl_news = view.findViewById(R.id.srl_news);
         srl_news.setColorSchemeColors(Color.parseColor("#ffce3d3a"));
-        presenter=new NewsPresenter(this);
         srl_news.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                presenter.loadNews(type,0);
+                presenter.loadNews(type, 0);
             }
         });
-
-        }
+        presenter.loadNews(type, 0);
+    }
 
     @Override
     public void showNews(final NewsBean newsBean) {
-    getActivity().runOnUiThread(new TimerTask() {
-    @Override
-    public void run() {
         switch (type) {
             case FgNewsFragment.NEW_TYPE_TOP:
-                tv_news.setText(newsBean.getTop().get(0).getTitle() + " "
-                        + newsBean.getTop().get(0).getMtime());
+                newsBeanList = newsBean.getTop();
                 break;
             case FgNewsFragment.NEW_TYPE_NBA:
-                tv_news.setText(newsBean.getNba().get(0).getTitle() + " "
-                        + newsBean.getNba().get(0).getMtime());
+                newsBeanList = newsBean.getNba();
                 break;
             case FgNewsFragment.NEW_TYPE_JOKES:
-                tv_news.setText(newsBean.getJoke().get(0).getTitle() + " "
-                        + newsBean.getJoke().get(0).getMtime());
+                newsBeanList = newsBean.getJoke();
                 break;
         }
-    }
-});
+        adapter.setData(newsBeanList);
+        rv_news.setLayoutManager(new LinearLayoutManager(getActivity(),
+                LinearLayoutManager.VERTICAL, false));
+        rv_news.setAdapter(adapter);
+        tv_news_list.setVisibility(View.GONE);
+
     }
 
     @Override
     public void hideDialog() {
-srl_news.setRefreshing(false);
+        srl_news.setRefreshing(false);
     }
 
     @Override
     public void showDialog() {
-srl_news.setRefreshing(true);
+        srl_news.setRefreshing(true);
     }
 
     @Override
     public void showErrorMsg(String error) {
-tv_news.setText("加载失败:"+error);
+        tv_news_list.setText("加载失败：" + error);
     }
 }
